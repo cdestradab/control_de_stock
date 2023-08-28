f0d6e6b271a42689ab2e6eb59843b30bba54af3c
Cristian Estrada <cdestradab@gmail.com>
2023-08-28 03:11:03 -0500

    proyecto listo

---
+++ control-de-stock-2/src/main/java/com/alura/jdbc/dao/CategoriaDAO.java
@@ -0,0 +1,90 @@
+package com.alura.jdbc.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.alura.jdbc.modelo.Categoria;
+import com.alura.jdbc.modelo.Producto;
+
+public class CategoriaDAO {
+	
+	private Connection con;
+
+	public CategoriaDAO(Connection con) {
+		this.con = con;
+	}
+
+	public List<Categoria> listar() {
+		List<Categoria> resultado = new ArrayList<>();
+		
+		try {
+			final PreparedStatement statement = con.prepareStatement(
+					"SELECT id, nombre FROM categoria");
+			
+			try(statement){
+				final ResultSet resultSet = statement.executeQuery();
+				
+				try (resultSet) {
+					while (resultSet.next()) {
+						var categoria = new Categoria(resultSet.getInt("ID"),
+								resultSet.getString("NOMBRE"));
+						
+						resultado.add(categoria);
+					}
+				};
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return resultado;
+	}
+
+	public List<Categoria> listarConProductos() {
+		List<Categoria> resultado = new ArrayList<>();
+		
+		try {
+			final PreparedStatement statement = con.prepareStatement(
+					"SELECT c.id, c.nombre, p.id, p.nombre, p.cantidad " 
+					+ "FROM categoria c "
+					+ "INNER JOIN producto p on c.id = p.categoria_id");
+			
+			try(statement){
+				final ResultSet resultSet = statement.executeQuery();
+				
+				try (resultSet) {
+					while (resultSet.next()) {
+						Integer categoriaId = resultSet.getInt("C.ID");
+						String categoriaNombre = resultSet.getString("C.NOMBRE");
+						
+						var categoria = resultado
+								.stream()
+								.filter(cat -> cat.getId().equals(categoriaId))
+								.findAny().orElseGet( () -> {
+									Categoria cat = new Categoria(categoriaId, categoriaNombre);
+									
+									resultado.add(cat);
+									
+									return cat;
+								});
+						
+						Producto producto = new Producto(resultSet.getInt("P.ID"),
+								resultSet.getString("P.NOMBRE"),
+								resultSet.getInt("P.CANTIDAD"));
+						
+						categoria.agregar(producto);
+					}
+				};
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return resultado;
+	}
+
+}
